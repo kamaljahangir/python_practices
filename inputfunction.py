2c7362ebd1ddb06d9727e5f7243eebfc00af8cc2
root <root@ip-172-31-24-34.ap-south-1.compute.internal>
2019-05-15 20:10:00 +0000

    User input through Input Function

---
+++ inputfunction.py
@@ -0,0 +1,8 @@
+#User input through Input function
+Name=input("Enter ur name")
+print(Name)
+Address=input("Enter ur Address")
+print(Address)
+Class=input("Enter class inwhich enroled")
+print(Class)
+

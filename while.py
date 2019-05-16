de0282cb21f0c5fcaf96f48411865a7347a16610
root <root@ip-172-31-11-201.ap-south-1.compute.internal>
2019-05-16 09:44:43 +0000

    while loop

---
+++ while.py
@@ -0,0 +1,18 @@
+# Searching object
+import sys, time
+import webbrowser   # deal with web browser
+
+data=sys.argv[1:]
+
+print(data)
+
+#searching object
+while true:
+	for i in data:
+		# code for web search
+		print(i)
+		time.sleep(3)
+		webbrowser.open_new_tab("http://www.google.com/search?q="+i)
+
+
+	time.sleep(5)

1bd6a238590c091557628ceb667c6914bc870930
root <root@ip-172-31-11-201.ap-south-1.compute.internal>
2019-05-16 08:42:25 +0000

    Addition of numbers through function

---
+++ addition.py
@@ -0,0 +1,9 @@
+def AddN(n):
+	s= sum(range(n+1))
+	return s
+
+print ("AddN(10) --> ", AddN(10))
+a = int(input("Enter any number: "))
+print ("AddN(a) --> ", AddN(a))
+print ("AddN(50) --> ", AddN(50))
+print ("AddN(200) --> ", AddN(200))
